4ed9facfdb0c4b89585d77c942f85a9b2d30e09a
Jack Surasak <jack@finnomena.com>
2018-10-04 17:37:24 +0700

    remove unused

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -76,13 +76,9 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
   },
   data: () => ({
     profile: {
